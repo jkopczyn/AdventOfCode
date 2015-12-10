c380dffcdb9f38465330d4dd4715f8d9642f7ed0
Jacob Kopczynski <ja.kopczynski@gmail.com>
2015-12-10 00:38:40 -0800

    new strategy

--- day7.rb
+++ day7.rb
@@ -1,24 +1,43 @@
 require 'byebug'
+
+OP_TOKENS = [:AND, :OR, :LSHIFT, :RSHIFT, :NOT]
+
 def wiring(filename)
   wire_map = Hash.new
   File.open(filename, 'r') do |file|
-    queue = file.each.map do |line|
+    file.each do |line|
       expression, wire = line.split(" -> ")
-      tokens = expression.split.map { |str| /^[0-9]*$/.match(str) ? str.to_i : str.to_sym }
-      [wire, tokens]
-    end.sort_by {|pair| pair[1].length}
-    until queue.empty?
-      wire, tokens = queue.dequeue
-      case tokens.length
-      when
-      when
-      when
+      wire_map[wire.to_sym] = expression.split.map do |str| 
+        /^[0-9]*$/.match(str) ? str.to_i : str.to_sym
+      end
+    end
+    stack = [:a]
+    until stack.empty?
+      wire = stack.pop
+      next if wire_map[wire].is_a?(Integer)
+      dependencies = wire_map[wire].reject do |token| 
+        token.is_a?(Integer) or OP_TOKENS.include?(token)
+      end
+      if dependencies.all { |other_wire| wire_map[other_wire].is_a?(Integer)}
+        #compute this value
+      else
+        stack.push(wire)
+        dependencies.each { |w| stack.push(wire) }
+        #this might end with a wire on the stack multiple times
       end
     end
   end
   debugger
   wire_map[:a]
 end
+
+#put each expression in a bucket
+#later expressions trump earlier ones
+#
+#treat as a stack? push on :a, if it requires others push it back in
+#and push the requirements in on top of it
+#
+
 
 def evaluate_expression(expression, wire_map)
   tokens = expression.split.map { |str| /^[0-9]*$/.match(str) ? str.to_i : str.to_sym }
