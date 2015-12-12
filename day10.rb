a52d641e8cb85a412223da56dd1d5948dedcb455
Jacob Kopczynski <ja.kopczynski@gmail.com>
2015-12-11 23:43:18 -0800

    day10 complete

--- day10.rb
+++ day10.rb
@@ -1,5 +1,32 @@
-def look_and_say(start_string, iterations=1)
+def look_and_say_once(start_string)
+  next_array = []
+  accum = ""
+  curr_char = ""
+  start_string.each_char do |chr|
+    curr_char = chr if curr_char.empty?
+    if chr == curr_char
+      accum += chr
+    else
+      #p accum
+      next_array.push(accum)
+      accum = curr_char = chr
+    end
+  end
+  next_array.push(accum)
+  next_string = next_array.map { |run| "#{run.length}#{run[0]}" }.join
+  #p next_array
+  next_string
 end
 
-puts function("input.txt")
- 
+def look_and_say(start_string, iterations=1)
+  iterations.times do
+    start_string = look_and_say_once(start_string)
+  end
+  start_string
+end
+str = "1321131112"
+
+#p look_and_say_once(str)
+x = look_and_say(str, 40)
+puts x.length
+puts look_and_say(x, 10).length
