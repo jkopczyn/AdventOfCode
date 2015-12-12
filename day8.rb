daaa22201b765bfcc70c49bac6bf9e98030ca066
Jacob Kopczynski <ja.kopczynski@gmail.com>
2015-12-11 21:42:35 -0800

    attempt at day 8 part 1

---
+++ day8.rb
@@ -0,0 +1,23 @@
+def count_extra_characters(filename)
+  count = 0
+  File.open(filename, 'r') do |file|
+    file.each do |line|
+      temp_counter = 0
+      line = line.strip
+      begin
+        /"(.*)"/.match(line)[1]
+        temp_counter += 2
+      rescue NoMethodError
+        next
+      end
+      temp_counter += 3*line.scan(/(\\x[0-9a-f]{2})/).length
+      temp_counter += line.scan(/\\\"|\\\\/).length
+      puts "#{line}, count: #{temp_counter}"
+      count += temp_counter
+    end
+  end
+  count
+end
+
+puts count_extra_characters("input8.txt")
+ 
