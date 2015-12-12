ad6c13c8a0b5f4ab0ce8e0e2468257c0a450e533
Jacob Kopczynski <ja.kopczynski@gmail.com>
2015-12-11 21:57:09 -0800

    account for overlapping patterns like 'a\xab'

--- day8.rb
+++ day8.rb
@@ -5,12 +5,16 @@ def count_extra_characters(filename)
       temp_counter = 0
       line = line.strip
       begin
-        /"(.*)"/.match(line)[1]
-        temp_counter += 2
+        if /"(.*)"/.match(line)[1].length == (line.length - 2)
+          temp_counter += 2 
+        else
+          next
+        end
       rescue NoMethodError
         next
       end
       temp_counter += 3*line.scan(/(\\x[0-9a-f]{2})/).length
+      temp_counter -= 3*line.scan(/(([^\\]|^)\\\\x[0-9a-f]{2})/).length
       temp_counter += line.scan(/\\\"|\\\\/).length
       puts "#{line}, count: #{temp_counter}"
       count += temp_counter
@@ -20,4 +24,5 @@ def count_extra_characters(filename)
 end
 
 puts count_extra_characters("input8.txt")
- 
+#min 1198 max 1345
+
