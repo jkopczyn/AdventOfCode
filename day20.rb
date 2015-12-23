5b897f6570b56bbc9ee5dc3e48f51a2ef0967de1
Jacob Kopczynski <ja.kopczynski@gmail.com>
2015-12-22 22:13:51 -0500

    lazy elves working

--- day20.rb
+++ day20.rb
@@ -13,7 +13,6 @@ def factors_of(number)
 end
 
 def sum_factors(target)
-  numbers = {0 => nil}
   max = target/10
   1.upto(max) do |n|
     tot = factors_of(n).inject(&:+)
@@ -22,4 +21,26 @@ def sum_factors(target)
   end
 end
 
-puts sum_factors(33100000)
+def lazy_divisors(number)
+  return [1] if number == 1
+  primes, powers = number.prime_division.transpose
+  exponents = powers.map{|i| (0..i).to_a}
+  divisors = exponents.shift.product(*exponents).map do |powers|
+    primes.zip(powers).map{|prime, power| prime ** power}.inject(:*)
+  end.select {|x| x*50 >= number }
+  divisors
+end
+
+def lazy_elves(target)
+  max = target/11
+  1.upto(max) do |n|
+    set = lazy_divisors(n)
+    tot = set.inject(&:+) unless set.nil?
+    puts "#{n}, #{tot}" if n%144 == 0
+    return n if tot >= max
+  end
+
+end
+
+#puts sum_factors(33100000)
+puts lazy_elves(33100000)
