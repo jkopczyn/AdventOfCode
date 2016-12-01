f2f356690e47b95afdd82a0b82b59d2b4f3cd688
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-01 09:03:07 -0800

    day1 part1 complete

--- 2016/day01.rb
+++ 2016/day01.rb
@@ -1,11 +1,22 @@
 require 'byebug'
 
 def twisting_grid(filename)
-  File.open(filename, 'r') do |file|
-      commands = file.split(", ")
-  end
-  vector = (0, 1) #x, y
-  position = (0, 0)
+    File.open(filename, 'r') do |file|
+        commands = file.read.split(", ")
+        vector = [0, 1] #x, y
+        position = [0, 0]
+        commands.each do |token|
+            results = parse_command(token)
+            vector = turn(vector, results[:turn])
+            position = advance(position, vector, results[:distance])
+            puts "#{token} brings us to #{position}"
+        end
+        position[0].abs + position[1].abs
+    end
+end
+
+def advance(position, vector, distance)
+    [position[0] + vector[0]*distance, position[1] + vector[1]*distance]
 end
 
 def parse_command(token)
@@ -17,11 +28,11 @@ def parse_command(token)
 end
 
 def turn(vector, direction)
-    if direction in [:right, :R, :r]
-        (vector[1], -vector[0])
-    elsif direction in [:left, :L, :l]
-        (-vector[1], vector[0])
+    if([:right, :R, :r].include?(direction))
+        [vector[1], -vector[0]]
+    elsif([:left, :L, :l].include?(direction))
+        [-vector[1], vector[0]]
     end
 end
 
-puts twisting_grid("input.txt")
+puts twisting_grid("input01.txt")
