1343be20e8f3fdc7bca339ded917d64e3e77789c
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-02 10:58:32 -0800

    location to button

--- 2016/day02.rb
+++ 2016/day02.rb
@@ -11,5 +11,25 @@ def function(filename)
   end
 end
 
+def location_to_number(location, center=[1,1])
+    x, y = location
+    relative_x, relative_y = (x - center[0]), (y - center[0])
+    if relative_x.abs > 1 or relative_y.abs > 1
+        raise "Out Of Bounds Exception"
+    end
+    case [relative_x, relative_y]
+    when [-1, -1] then 1
+    when [0, -1]  then 2
+    when [1, -1]  then 3
+    when [-1, 0]  then 4
+    when [0, 0]   then 5
+    when [1, 0]   then 6
+    when [-1, 1]  then 7
+    when [0, 1]   then 8
+    when [1, 1]   then 9
+    end
+
+end
+
 puts function("input.txt")
  
