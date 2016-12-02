969fa95af1a64a6085e7b0c8187da9e18e3f2a0a
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-02 11:42:24 -0800

    fix boundary check and orientation of buttons

--- 2016/day02.rb
+++ 2016/day02.rb
@@ -14,16 +14,43 @@ def function(filename)
 end
 
 def chain_moves(moves, location, center=CENTER)
+    current_location = location
+    moves.each do |move|
+        vector = decode_movement(move)
+        next_location = [
+            vector[0]+current_location[0],
+            vector[1]+current_location[1]
+        ]
+        unless out_of_bounds?(relativize(next_location,center))
+            current_location = next_location 
+        end
+    end
+    current_location
+end
 
+def decode_movement(token)
+    case token
+    when 'U' then [0,1]
+    when 'D' then [0,-1]
+    when 'L' then [-1,0]
+    when 'R' then [1,0]
+    else raise "Argument Error"
+    end
+end
+
+def relativize(location, center)
+    [(location[0] - center[0]), (location[1] - center[1])]
+end
+
+def out_of_bounds?(relative_location)
+    relative_x, relative_y = relative_location
+    not [relative_x, relative_y].all? { |coord| [-1, 0, 1].include?(coord.abs) }
 end
 
 def location_to_number(location, center=CENTER)
-    x, y = location
-    relative_x, relative_y = (x - center[0]), (y - center[0])
-    unless [-1, 0, 1].include?(relative_x.abs) and [-1, 0, 1].include?(relative_y.abs)
-        raise "Out Of Bounds Exception"
-    end
-    5 + relative_x + 3*relative_y
+    relative_x, relative_y = relativize(location, center)
+    raise "Out Of Bounds Exception" if out_of_bounds?([relative_x, relative_y])
+    5 + relative_x - 3*relative_y
 end
 
 puts function("input.txt")
