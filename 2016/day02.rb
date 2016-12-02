d08a2ee5bdd407d4e3dcaf7bb1aac73240448bc9
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-02 11:09:07 -0800

    refactor button finding

--- 2016/day02.rb
+++ 2016/day02.rb
@@ -14,22 +14,14 @@ end
 def location_to_number(location, center=[1,1])
     x, y = location
     relative_x, relative_y = (x - center[0]), (y - center[0])
-    if relative_x.abs > 1 or relative_y.abs > 1
+    unless [-1, 0, 1].include?(relative_x.abs) and [-1, 0, 1].include?(relative_y.abs)
         raise "Out Of Bounds Exception"
     end
-    case [relative_x, relative_y]
-    when [-1, -1] then 1
-    when [0, -1]  then 2
-    when [1, -1]  then 3
-    when [-1, 0]  then 4
-    when [0, 0]   then 5
-    when [1, 0]   then 6
-    when [-1, 1]  then 7
-    when [0, 1]   then 8
-    when [1, 1]   then 9
+    5 + relative_x + 3*relative_y
     end
 
-end
-
-puts function("input.txt")
+#loc = [0,1,2].map { |x| [0,1,2].map { |y| [x, y] }}.flatten(1).shuffle
+#p loc
+#p loc.map { |p| location_to_number(p) }
+#puts function("input.txt")
  
