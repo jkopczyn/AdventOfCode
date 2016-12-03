6d2f0a21cf42e0b46e3575d58ad306a4677093af
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-02 22:21:35 -0800

    part 2, vertical triples, done

--- 2016/day03.rb
+++ 2016/day03.rb
@@ -7,12 +7,17 @@ def function(filename)
       file.each do |line|
           storage.push(line.split.map(&:to_i))
           if storage.length == 3
-              triples.extend(mixup(storage))
+              triples += mixup(storage)
               storage = []
           end
       end
       triples.select {|trip| valid_triangle?(trip) }.count
   end
+end
+
+def mixup(three_by_three)
+    r0, r1, r2 = three_by_three
+    [[r0[0], r1[0], r2[0]], [r0[1], r1[1], r2[1]], [r0[2], r1[2], r2[2]]]
 end
 
 def valid_triangle?(triple)
