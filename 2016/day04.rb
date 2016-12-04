d06513fa8f4bc3e22c880210c36eb863f23eebf3
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-03 21:19:24 -0800

    basics of counting line

--- 2016/day04.rb
+++ 2016/day04.rb
@@ -3,8 +3,15 @@ require 'byebug'
 def function(filename)
   File.open(filename, 'r') do |file|
     file.each do |line|
+        valid_room?(line)
     end
   end
+end
+
+def valid_room?(label)
+    prefix, checksum = label.split(/[\[\]]/)
+    counter = Hash.new(0)
+    prefix.split('').select { |c| /[a-z]/.match(c) }.each { |c| counter[c] += 1 }
 end
 
 puts function("input.txt")
