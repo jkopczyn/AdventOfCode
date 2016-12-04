e1e82ee54020e02ea6cff9c31c2687bdb73d8680
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-03 21:27:51 -0800

    convert to number

--- 2016/day04.rb
+++ 2016/day04.rb
@@ -3,7 +3,7 @@ require 'byebug'
 def function(filename)
   File.open(filename, 'r') do |file|
     file.each do |line|
-        valid_room?(line)
+        puts valid_room?(line)
     end
   end
 end
@@ -12,6 +12,11 @@ def valid_room?(label)
     prefix, checksum = label.split(/[\[\]]/)
     counter = Hash.new(0)
     prefix.split('').select { |c| /[a-z]/.match(c) }.each { |c| counter[c] += 1 }
+    if counter.keys.sort_by { |k| [-counter[k], k] }[0..4].join('') == checksum
+        prefix.rpartition('-').last.to_i
+    else
+        0
+    end
 end
 
 puts function("input.txt")
