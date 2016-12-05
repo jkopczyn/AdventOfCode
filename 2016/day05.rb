8355c14046e1044da407c9dda4104407d4f85f7d
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-04 21:28:17 -0800

    refactor

--- 2016/day05.rb
+++ 2016/day05.rb
@@ -6,13 +6,24 @@ def function(id)
     output = ['']*8
     while output.select { |s| s != '' }.length < 8
         hash = Digest::MD5.hexdigest(id+i.to_s)
-        if hash[0..4] == "00000" and is_decimal(hash[5]) and output[is_decimal(hash[5])] == ''
+        if hash[0..4] == "00000" and valid_index(output, hash[5])
             output[hash[5].to_i] = hash[6]
             p i, output
         end
         i += 1
     end
     output.join('')
+end
+
+def valid_index(array, char)
+    digit = is_decimal(char)
+    if digit
+        blank_at_index(array, digit)
+    end
+end
+
+def blank_at_index(array, idx)
+    output[idx] == ''
 end
 
 def is_decimal(char)
