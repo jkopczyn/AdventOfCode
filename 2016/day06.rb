5d45a0a8f5c38adfa8da811dff199138cb728a0f
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-05 21:40:58 -0800

    the two parts were basically the same?

--- 2016/day06.rb
+++ 2016/day06.rb
@@ -1,11 +1,24 @@
 require 'byebug'
 
 def function(filename)
+  character_map = []
   File.open(filename, 'r') do |file|
     file.each do |line|
+        if character_map.empty?
+            character_map = line.split('').map { |c| [c] }
+        else
+            line.split('').each_with_index { |c, idx| character_map[idx] << c }
+        end
     end
   end
+  character_map.map { |list| majority(list) }.join('')
 end
 
-puts function("input.txt")
+def majority(characters)
+    multiset = Hash.new(0)
+    characters.each { |c| multiset[c] += 1 }
+    multiset.keys.sort_by { |c| -multiset[c] }.last
+end
+
+puts function("input06.txt")
  
