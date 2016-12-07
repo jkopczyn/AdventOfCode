5a4917dc12c5afbbaf2fc190f925fbf32072b003
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-06 21:20:23 -0800

    part 1 complete

--- 2016/day07.rb
+++ 2016/day07.rb
@@ -4,7 +4,7 @@ def function(filename)
   File.open(filename, 'r') do |file|
     file.map do |line|
         valid_tls(line) ? 1 : 0
-    end
+    end.inject(&:+)
   end
 end
 
@@ -12,5 +12,5 @@ def valid_tls(string)
    /([a-z])((?!\1)[a-z])\2\1/.match(string) and not /[\w]*\[[\w]*?([a-z])((?!\1)[a-z])\2\1[\w]*?\][\w]*/.match(string) 
 end
 
-puts function("input.txt")
+puts function("input07.txt")
  
