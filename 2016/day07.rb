e0f0632acacd7533083ac2c34c1e9867291482e2
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-06 21:45:44 -0800

    part 2 stuff, needs overlapping scans

--- 2016/day07.rb
+++ 2016/day07.rb
@@ -3,7 +3,7 @@ require 'byebug'
 def function(filename)
   File.open(filename, 'r') do |file|
     file.map do |line|
-        valid_tls(line) ? 1 : 0
+        valid_ssl(line) ? 1 : 0
     end.inject(&:+)
   end
 end
@@ -12,5 +12,14 @@ def valid_tls(string)
    /([a-z])((?!\1)[a-z])\2\1/.match(string) and not /[\w]*\[[\w]*?([a-z])((?!\1)[a-z])\2\1[\w]*?\][\w]*/.match(string) 
 end
 
-puts function("input07.txt")
- 
+def valid_ssl(string)
+    pieces = string.split(/[\[\]]/)
+    outside = pieces.select.each_with_index { |_, i| i.even? }
+    inside  = pieces.select.each_with_index { |_, i| i.odd? }
+    babs = outside.map do |substring| 
+        substring.scan(/(([a-z])((?!\2)[a-z])\2)/) 
+    end.flatten(1).map {|arr| arr[2]+arr[1]+arr[2] }
+    p inside, babs
+    inside.any? {|substring| babs.any? {|bab| not substring.scan(bab).empty? }}
+end
+puts function("input.txt")
