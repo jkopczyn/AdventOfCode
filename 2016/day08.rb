0d35f4d4303651ed0bf4ad9225e78645559d7b44
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-07 21:40:48 -0800

    checking the actual thing

--- 2016/day08.rb
+++ 2016/day08.rb
@@ -17,10 +17,10 @@ def parse(command)
     when "rect"
         [:rect] + tokens[1].split('x').map(&:to_i)
     when "rotate"
-        case tokens[1]
-        when "column"
+        case tokens[2][0]
+        when "x"
             [:column, tokens[2][-1].to_i, tokens.last.to_i]
-        when "row"
+        when "y"
             [:row, tokens[2][-1].to_i, tokens.last.to_i]
         end
     end
