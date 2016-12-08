fc5909a3cc1c7c297ae92b97c1a01ae84f6c3bbc
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-07 21:39:17 -0800

    parsing

--- 2016/day08.rb
+++ 2016/day08.rb
@@ -6,12 +6,24 @@ def function(filename)
     file.each do |line|
         screen = do_op(screen, parse(line))
     end
+    print_screen(screen)
     screen.map {|row| row.map {|v| v ? 1 : 0 }.inject(&:+) }.inject(&:+)
   end
 end
 
 def parse(command)
-    [:rect, 1, 1]
+    tokens = command.split
+    case tokens[0]
+    when "rect"
+        [:rect] + tokens[1].split('x').map(&:to_i)
+    when "rotate"
+        case tokens[1]
+        when "column"
+            [:column, tokens[2][-1].to_i, tokens.last.to_i]
+        when "row"
+            [:row, tokens[2][-1].to_i, tokens.last.to_i]
+        end
+    end
 end
 
 def do_op(screen, command)
@@ -27,7 +39,7 @@ def do_op(screen, command)
         x, drop = command[1..2]
         column = screen.map { |line| line[x] }
         column.length.times { |row|
-            screen[row][x] = column[(row - drop) % height] }
+            screen[row][x] = column[(row - drop) % column.length] }
     when :row
         y, shift = command[1..2]
         row = screen[y].dup
@@ -37,6 +49,12 @@ def do_op(screen, command)
         raise "command???"
     end
     screen
+end
+
+def print_screen(screen)
+    screen.each do |line|
+        puts line.map { |v| v ? '#' : '.' }.join('')
+    end
 end
 
 puts function("input.txt")
