d1c791d9902a2bdbb5ca3f672c82f1284d3322e0
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-08 22:13:32 -0800

    cleaner call tree

--- 2016/day09.rb
+++ 2016/day09.rb
@@ -2,17 +2,16 @@ require 'byebug'
 
 def function(filename)
     File.open(filename, 'r') do |file|
-        decompress(file.read.strip)
+        scan_step(file.read.strip)
     end
 end
 
-def scan_step(string, decompressed_length=0)
+def scan_step(string)
     left = string.index('(')
-    return decompressed_length + string.length unless left
+    return string.length unless left
     right = string.index(')', left)
     scan_length, repeats = string[left+1...right].split('x').map(&:to_i)
-    scan_step(string[right+scan_length+1..-1],
-              decompressed_length+left+scan_length*repeats)
+    left + scan_length*repeats + scan_step(string[right+scan_length+1..-1])
 end
 
 def decompress(string)
@@ -20,8 +19,7 @@ def decompress(string)
     return string.length if left.nil?
     right = string.index(')', left)
     scan_length, repeats = string[left+1...right].split('x').map(&:to_i)
-    left +
-        repeats * decompress(string[right+1..right+scan_length]) +
+    left + repeats * decompress(string[right+1..right+scan_length]) +
         decompress(string[right+scan_length+1..-1])
 end
 
