842e32cca1f37ef248e6c855fac23a417d5771c6
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-08 23:03:33 -0800

    refactor the two parts into one

--- 2016/day09.rb
+++ 2016/day09.rb
@@ -2,25 +2,21 @@ require 'byebug'
 
 def function(filename)
     File.open(filename, 'r') do |file|
-        scan_step(file.read.strip)
+        decompress(file.read.strip, recursive=true)
     end
 end
 
-def scan_step(string)
-    left = string.index('(')
-    return string.length unless left
-    right = string.index(')', left)
-    scan_length, repeats = string[left+1...right].split('x').map(&:to_i)
-    left + scan_length*repeats + scan_step(string[right+scan_length+1..-1])
-end
-
-def decompress(string)
+def decompress(string, recursive=false)
     left = string.index('(')
     return string.length if left.nil?
     right = string.index(')', left)
     scan_length, repeats = string[left+1...right].split('x').map(&:to_i)
-    left + repeats * decompress(string[right+1..right+scan_length]) +
-        decompress(string[right+scan_length+1..-1])
+    if recursive
+        left + repeats * decompress(string[right+1..right+scan_length], true) +
+            decompress(string[right+scan_length+1..-1], true)
+    else
+        left + scan_length*repeats + decompress(string[right+scan_length+1..-1])
+    end
 end
 
 
