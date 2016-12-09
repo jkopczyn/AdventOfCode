e607652a6aa28f0c2039e9a8be4c5108e506f676
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-08 22:08:26 -0800

    even more cleanup

--- 2016/day09.rb
+++ 2016/day09.rb
@@ -2,20 +2,17 @@ require 'byebug'
 
 def function(filename)
     File.open(filename, 'r') do |file|
-        scan_step(file.read.strip)
+        decompress(file.read.strip)
     end
 end
 
 def scan_step(string, decompressed_length=0)
-    return decompressed_length if not string
     left = string.index('(')
     return decompressed_length + string.length unless left
     right = string.index(')', left)
-    decompressed_length += left
     scan_length, repeats = string[left+1...right].split('x').map(&:to_i)
-    string = string[right+1..-1]
-    decompressed_length += scan_length*repeats
-    scan_step(string[scan_length..-1], decompressed_length)
+    scan_step(string[right+scan_length+1..-1],
+              decompressed_length+left+scan_length*repeats)
 end
 
 def decompress(string)
@@ -23,7 +20,9 @@ def decompress(string)
     return string.length if left.nil?
     right = string.index(')', left)
     scan_length, repeats = string[left+1...right].split('x').map(&:to_i)
-    left + repeats * decompress(string[right+1..right+scan_length]) + decompress(string[right+scan_length+1..-1])
+    left +
+        repeats * decompress(string[right+1..right+scan_length]) +
+        decompress(string[right+scan_length+1..-1])
 end
 
 
