690be8ba7a038a4fb4aa83a28646a8ff30d03cc7
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-10 18:20:00 -0800

    check for whether a bot is fully loaded

--- 2016/day10.rb
+++ 2016/day10.rb
@@ -1,12 +1,25 @@
 require 'byebug'
 
 def function(filename)
+    bot_map = Hash.new { |k| { number: k, current: [], low: nil, high: nil } }
     File.open(filename, 'r') do |file|
-        #file.read.strip
         file.each do |line|
             ''
         end
     end
 end
+
+def check_bot(bots, bot)
+    if bot[:current].length == 2
+        if bot[:current].sort == [17, 61]
+            return { solution: bot[:number] }
+        end
+        low, high = bot[:current].sort
+        move_chip(bots, bot[:low], low)
+        move_chip(bots, bot[:high], high)
+        bot[:current] = []
+    end
+end
+
 
 puts function("input.txt")
