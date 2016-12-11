d6fe2353ce708338a83060a4c3c968f323736513
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-10 19:16:44 -0800

    parsing lines

--- 2016/day10.rb
+++ 2016/day10.rb
@@ -4,13 +4,30 @@ def function(filename)
     bot_map = Hash.new { |k| { number: k, current: [], low: nil, high: nil } }
     File.open(filename, 'r') do |file|
         file.each do |line|
-            ''
+            result = main_loop(bot_map, parse_line(line))
+            return result if result
         end
     end
 end
 
 def parse_line(string)
+    tokens = string.split
+    case tokens[0]
+    when "value"
+        {destination: tokens[-1].to_i, value: tokens[1].to_i}
+    when "bot"
+        {rule: true, bot: tokens[1].to_i,
+         low: label(tokens[5..6]), high: label(tokens[10..11])}
+    end
+end
 
+def label(pair)
+    case pair[0]
+    when "bot"
+        pair[1].to_i
+    when "output"
+        pair.join('').to_sym
+    end
 end
 
 def main_loop(bots, input)
@@ -27,6 +44,7 @@ def main_loop(bots, input)
         bot = bots[input[:bot]]
         bot[:low], bot[:high] = input[:low], input[:high]
     end
+    nil
 end
 
 def check_bot(bots, bot)
