02488f7a2932953ae5b3149376b18fd8f297d274
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-10 21:15:33 -0800

    basic line parsing

--- 2016/day11.rb
+++ 2016/day11.rb
@@ -1,12 +1,19 @@
 require 'byebug'
+require 'set'
 
 def function(filename)
     File.open(filename, 'r') do |file|
         #file.read.strip
-        file.each do |line|
-            ''
-        end
+        file.each_with_index.map do |line, idx|
+            [1+idx, parse_line(line)]
+        end.to_h
     end
+end
+
+def parse_line(line)
+    items = line.split(/contains|\./)[1].split(/,|and/).map(&:strip)
+    floor = { generators: Set.new, chips: Set.new }
+    return floor if items.first == "nothing relevant"
 end
 
 puts function("input.txt")
