2924fa6f25285298f8d47bf1c8669f2e0e18f55a
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-10 21:33:21 -0800

    safety check for each intermediate state

--- 2016/day11.rb
+++ 2016/day11.rb
@@ -3,11 +3,26 @@ require 'set'
 
 def function(filename)
     File.open(filename, 'r') do |file|
-        #file.read.strip
-        file.each_with_index.map do |line, idx|
+        building = file.each_with_index.map do |line, idx|
             [1+idx, parse_line(line)]
         end.to_h
+        explore_solutions(building)
     end
+end
+
+def explore_solutions(building)
+    [1].each do
+        next if is_unsafe?(building)
+    end
+    building
+end
+
+def is_unsafe?(building)
+    building.values.any? { |floor| not chips_safe?(floor) }
+end
+
+def chips_safe?(floor)
+    floor[:microchip].empty? or ((floor[:generator] - floor[:microchip]).empty?)
 end
 
 def parse_line(line)
@@ -15,6 +30,7 @@ def parse_line(line)
     floor = { generator: Set.new, microchip: Set.new }
     return floor if items.first == "nothing relevant"
     items.each do |item|
+        _,  element, thing = item.split
         element, thing = element.split('-')[0].to_sym, thing.to_sym
         floor[thing].add(element)
     end
