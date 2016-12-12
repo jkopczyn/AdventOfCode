935c40810d440bcd181e0769ffcd4a34c212776f
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-11 21:09:41 -0800

    scaffold for register machine

--- 2016/day12.rb
+++ 2016/day12.rb
@@ -3,10 +3,31 @@ require 'byebug'
 def function(filename)
     File.open(filename, 'r') do |file|
         #file.read.strip
-        file.each do |line|
-            ''
+        commands = file.map do |line|
+            parse(line)
+        end
+        state = {registers: {a: 0, b: 0, c: 0, d:0}, commands: commands, idx: 0}
+        until finished?(state)
+            state = evolve(state)
         end
     end
+    state
 end
 
-puts function("input.txt")
+def parse(command)
+    type, x, y = line.split
+end
+
+def finished?(state)
+    state[:idx] >= state[:commands].length
+end
+
+def evolve(state)
+
+end
+
+def execute(state, instruction)
+
+end
+
+p function("input.txt")
