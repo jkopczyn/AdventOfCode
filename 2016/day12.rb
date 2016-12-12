c3354a415a7999e4d378e49c409aadd9875266c0
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-12-11 21:14:35 -0800

    stub and debugging

--- 2016/day12.rb
+++ 2016/day12.rb
@@ -1,11 +1,10 @@
 require 'byebug'
 
 def function(filename)
+    state = {}
     File.open(filename, 'r') do |file|
         #file.read.strip
-        commands = file.map do |line|
-            parse(line)
-        end
+        commands = file.map { |line| parse(line) }
         state = {registers: {a: 0, b: 0, c: 0, d:0}, commands: commands, idx: 0}
         until finished?(state)
             state = evolve(state)
@@ -15,7 +14,9 @@ def function(filename)
 end
 
 def parse(command)
-    type, x, y = line.split
+    type, x, y = command.split
+    y = 0 if y.nil?
+    [type.to_sym, x.to_i, y.to_i]
 end
 
 def finished?(state)
@@ -23,7 +24,7 @@ def finished?(state)
 end
 
 def evolve(state)
-
+    state
 end
 
 def execute(state, instruction)
