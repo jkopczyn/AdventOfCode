b33459db3d5b9ae5c1f277462c2a9fe82ca5f98f
Jacob Kopczynski <ja.kopczynski@gmail.com>
2017-12-01 21:04:04 -0800

    day 1 part 2

--- 2017/day01.rb
+++ 2017/day01.rb
@@ -1,13 +1,17 @@
 require 'byebug'
 
-def function(filename)
+def function(filename, offset=nil)
     File.open(filename, 'r') do |file|
         digits = file.read.strip.split('').map(&:to_i)
+        unless offset
+             offset = digits.length/2
+        end
         selected_digits = digits.select.each_with_index do |el, idx|
-            el == digits[idx-1]
+            el == digits[idx-offset]
         end
         return selected_digits.inject(&:+)
     end
 end
 
+puts function("input01.txt", 1)
 puts function("input01.txt")
