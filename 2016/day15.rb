dd3af7496f622d00311e8f183c3351d96bbd7913
Jacob Kopczynski <ja.kopczynski@gmail.com>
2017-12-03 16:21:05 -0800

    2016 day15 part2

--- 2016/day15.rb
+++ 2016/day15.rb
@@ -1,12 +1,14 @@
 require 'byebug'
 
-def function(filename)
+def function(filename, extra=nil)
     File.open(filename, 'r') do |file|
         #file.read.strip
         discs = file.each.map do |line|
             /Disc #.* has (.*) positions; at time=0, it is at position (.*)\./.match(
-                "Disc #1 has 5 positions; at time=0, it is at position 4."
-            ).to_a[1..-1]
+                line).to_a[1..-1]
+        end
+        if extra
+            discs.push(extra)
         end
         lcm = 1
         discs.map!.each_with_index do |pair, idx|
@@ -14,6 +16,7 @@ def function(filename)
             lcm *= cycle
             [cycle, (position + idx + 1) % cycle]
         end
+        p discs
         0.upto(lcm) do |idx|
             return idx if discs.all? { |pair| (pair[1] + idx) % pair[0] == 0 }
         end
@@ -21,3 +24,6 @@ def function(filename)
 end
 
 puts function("input.txt")
+puts function("input.txt", [11,0])
+puts function("input15.txt")
+puts function("input15.txt", [11, 0])
