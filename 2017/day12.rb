224becdfd00feb13123b113aa576826ec05b1a54
Jacob Kopczynski <ja.kopczynski@gmail.com>
2017-12-12 22:40:39 -0800

    day12 part1

--- 2017/day12.rb
+++ 2017/day12.rb
@@ -2,11 +2,26 @@ require 'byebug'
 
 def function(filename)
     File.open(filename, 'r') do |file|
-        #file.read.strip
+        pipes = Hash.new { |h, k| h[k] = [] }
         file.each do |line|
-            ''
+            key, points = line.split('<->').map(&:strip)
+            points = points.split(',').map(&:strip)
+            points.push(key)
+            points.each { |point| pipes[point] += points }
         end
+        pipes.size.times do
+            unchanged = true
+            pipes.keys.each do |k|
+                size = pipes[k].size
+                pipes[k].uniq!.sort!
+                unchanged |= (size == pipes[k].size)
+                pipes[k].each { |v| pipes[v] += pipes[k] }
+            end
+            break if unchanged
+        end
+        return pipes['0'].uniq!.size
     end
 end
 
 puts function("input.txt")
+puts function("input12.txt")
