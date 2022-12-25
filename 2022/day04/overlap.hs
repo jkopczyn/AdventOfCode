5885c98888d7a3e4a9163ba9bc8a85d26487f544
Jacob Kopczynski <ja.kopczynski@gmail.com>
2022-12-25 16:46:39 -0500

    day4 pt. 1

---
+++ 2022/day04/overlap.hs
@@ -0,0 +1,44 @@
+import System.IO
+import Data.List.Split
+
+main = do  
+    contents <- getContents  
+    putStr (processLines contents)  
+
+processLines :: String -> String
+processLines input = unlines (business (lines input))
+
+business :: [String] -> [String]
+business xs = [show (realBusiness (linesToCoords xs))]
+
+-- realBusiness :: [Integer] -> [Integer]
+realBusiness xs = length (filter sharedMatchesEither xs)
+
+sharedMatchesEither :: ((Integer, Integer), (Integer, Integer)) -> Bool
+sharedMatchesEither (x, y) = let shared = (intersect (x, y))
+                             in ((x ==  shared) || (y == shared))
+
+
+linesToCoords :: [String] -> [((Integer, Integer), (Integer, Integer))]
+linesToCoords [] = []
+linesToCoords (s:ss) = (twoPairs (lineToPairs s)):(linesToCoords ss)
+
+intersect :: ((Integer, Integer), (Integer, Integer)) -> (Integer, Integer)
+intersect (x, y) = ((max (fst x) (fst y)), (min (snd x) (snd y)))
+
+twoPairs :: [(Integer, Integer)] -> ((Integer, Integer), (Integer, Integer))
+twoPairs [xs, ys] = (xs, ys)
+twoPairs _ = error "arg not a two-element list"
+
+lineToPairs :: String -> [(Integer, Integer)]
+lineToPairs str = map coords (splitOneOf "," str)
+
+coords :: String -> (Integer, Integer)
+coords co = case (splitOneOf "-" co) of [x,y] -> (read x, read y)
+                                        []    -> error "empty coordinates"
+                                        _     -> error "coordinates wrong length"
+
+
+intsToLines :: [Integer] -> [String]
+intsToLines [] = []
+intsToLines (x:xs) = (show x):(intsToLines xs)
