83905fde92c50a532fbda684a6ebd4c399e697d4
Jacob Kopczynski <ja.kopczynski@gmail.com>
2023-12-12 19:24:10 -0800

    test works day 1

--- 2023/01/main.py
+++ 2023/01/main.py
@@ -13,9 +13,23 @@ def main():
             metavar="OUTPUT_FILE", type=argparse.FileType("w"),
             help="path to the output file (write to stdout if omitted)")
     args = parser.parse_args()
+    total = 0
     for line in args.input:
-        print(line.strip(), file=args.output)
- 
+        total += numerals(line)
+    print(total, file=args.output)
+
+def numerals(line):
+    first = -1
+    last = -1
+    for c in line:
+        if c in "0123456789":
+            if first < 0:
+                first = int(c)
+                last = first
+            else:
+                last = int(c)
+    return 10*first+last
+
 
 if __name__ == "__main__":
     main()
