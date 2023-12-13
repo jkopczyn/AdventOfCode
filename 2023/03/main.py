dab399815738279cf7b7f294c4a8051c389ae3e5
Jacob Kopczynski <ja.kopczynski@gmail.com>
2023-12-12 22:41:07 -0800

    day 3 initial approach

--- 2023/03/main.py
+++ 2023/03/main.py
@@ -13,8 +13,28 @@ def main():
             metavar="OUTPUT_FILE", type=argparse.FileType("w"),
             help="path to the output file (write to stdout if omitted)")
     args = parser.parse_args()
+    lines = []
+    grid = []
+    totalpart = 0
     for line in args.input:
-        print(line.strip(), file=args.output)
+        lines.append(line.strip())
+        grid.append(list(line.strip()))
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            if grid[y][x] not in "0123456789.":
+                newgrid, parts = select(grid, x, y)
+                print(parts, file=args.output)
+                for part in parts:
+                    totalpart += part
+                grid = newgrid
+    print("", file=args.output)
+
+def select(grid, x, y):
+    # look above
+    # look left
+    # look right
+    # look below
+    return grid, []
  
 
 if __name__ == "__main__":
