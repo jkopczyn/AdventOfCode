f78ae0d46ed51631d43a550e60441760a9de3a5a
Jacob Kopczynski <ja.kopczynski@gmail.com>
2023-12-14 12:29:19 -0800

    day 7 score hands

--- 2023/07/main.py
+++ 2023/07/main.py
@@ -14,8 +14,50 @@ def main():
             help="path to the output file (write to stdout if omitted)")
     args = parser.parse_args()
     for line in args.input:
-        print(line.strip(), file=args.output)
- 
+        print(line, score_hand(line.split(" ")[0]), file=args.output)
+
+def score_hand(chars):
+    cards = {}
+    for c in chars:
+        if c in cards:
+            cards[c] += 1
+        else:
+            cards[c] = 1
+    counts = sorted(cards.values(), reverse=True)
+    first = counts[0]
+    second = counts[1]
+    if first == 1:
+        score = 0
+    elif first == 2 and second == 1:
+        score = 10000000000
+    elif first == 2 and second == 2:
+        score = 20000000000
+    elif first == 3 and second == 1:
+        score = 30000000000
+    elif first == 3 and second == 2:
+        score = 40000000000
+    elif first == 4:
+        score = 50000000000
+    elif first == 5:
+        score = 60000000000
+    else:
+        raise Exception(chars +"invalid score")
+    return score + tiebreaker(chars)
+
+def tiebreaker(chars):
+    s = ""
+    for c in chars:
+        if c in "123456789":
+            s += "0"+c
+        elif c == "T":
+            s += "10"
+        elif c == "J":
+            s += "11"
+        elif c == "Q":
+            s += "12"
+        elif c == "K":
+            s += "13"
+    return int(s)
 
 if __name__ == "__main__":
     main()
