6d3694e040ee33ef1be775603c49431e2691aedf
Jacob Kopczynski <ja.kopczynski@gmail.com>
2023-12-14 13:04:37 -0800

    day 8 initial

---
+++ 2023/08/main.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+import sys
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+            "input", nargs="?", default="-",
+            metavar="INPUT_FILE", type=argparse.FileType("r"),
+            help="path to the input file (read from stdin if omitted)")
+    parser.add_argument(
+            "output", nargs="?", default="-",
+            metavar="OUTPUT_FILE", type=argparse.FileType("w"),
+            help="path to the output file (write to stdout if omitted)")
+    args = parser.parse_args()
+    moves = args.input.readline().strip()
+    _ = args.input.readline()
+    print(moves, "moves")
+    for line in args.input:
+        print(line.strip(), file=args.output)
+ 
+
+if __name__ == "__main__":
+    main()
+
