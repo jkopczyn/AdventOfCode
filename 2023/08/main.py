32b48befd24c7729f674cfd01745f04ccc21f07a
Jacob Kopczynski <ja.kopczynski@gmail.com>
2023-12-14 14:16:57 -0800

    day 8 pt2 input works
    LCM things did

--- 2023/08/main.py
+++ 2023/08/main.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 import sys
 import argparse
+import math
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
@@ -24,19 +25,27 @@ def main():
         lefts[key] = left
         rights[key] = right
     locations = starts(lefts)
+    distances = [0]*len(locations)
     steps = 0
-    while not done(locations):
+    while True:
         step = moves[(steps % len(moves))]
         steps += 1
         newlocs = []
-        for location in locations:
+        for idx in range(len(locations)):
+            location = locations[idx]
             if step == "L":
                 newlocs.append(lefts[location])
             elif step == "R":
                 newlocs.append(rights[location])
             else:
                 raise Exception(step, "is an invalid step")
+            newloc = newlocs[-1]
+            if distances[idx] == 0 and newloc[-1] == "Z":
+                distances[idx] = steps
+        if len(list(x for x in distances if x == 0)) == 0:
+            break
         locations = newlocs
+    print(math.lcm(*distances))
     print(steps)
 
 def starts(paths):
