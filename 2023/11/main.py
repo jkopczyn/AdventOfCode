0996fcf817880d7a23546c1bf2bd51b7009d5ef7
Jacob Kopczynski <ja.kopczynski@gmail.com>
2023-12-15 13:04:40 -0800

    day 11 locate galaxies

--- 2023/11/main.py
+++ 2023/11/main.py
@@ -13,11 +13,37 @@ def main():
             metavar="OUTPUT_FILE", type=argparse.FileType("w"),
             help="path to the output file (write to stdout if omitted)")
     args = parser.parse_args()
-    initial = args.input.readline().strip()
-    dropline = args.input.readline()
-    print(initial, "initial")
+    galaxies = []
+    xs = {}
+    ys = {}
+    y = 0
     for line in args.input:
-        print(line.strip(), file=args.output)
+        ys[y] = False
+        for idx in range(len(line)):
+            if idx not in xs:
+                xs[idx] = False
+            if line[idx] == "#":
+                xs[idx] = True
+                ys[y] = True
+                galaxies.append((idx, y))
+        y += 1
+    blank_xs = list(x for x in xs if not xs[x])
+    blank_ys = list(y for y in ys if not ys[y])
+
+    extra_xs = [0]*len(xs)
+    for x in blank_xs:
+        for idx in range(x,len(xs)):
+            extra_xs[idx] += 1
+    xs_old_to_new = list(idx + extra_xs[idx] for idx in range(len(xs)))
+
+    extra_ys = [0]*len(ys)
+    for y in blank_ys:
+        for idx in range(y,len(ys)):
+            extra_ys[idx] += 1
+    ys_old_to_new = list(idx + extra_ys[idx] for idx in range(len(ys)))
+
+    new_galaxies = list((xs_old_to_new[g[0]], ys_old_to_new[g[1]]) for g in galaxies)
+    print(new_galaxies, file=args.output)
  
 
 if __name__ == "__main__":
