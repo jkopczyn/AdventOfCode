787c3421987f8fbc73d182d877850e247d7b022c
Jacob Kopczynski <ja.kopczynski@gmail.com>
2023-12-15 13:56:12 -0800

    day 13 check mirror splits rows and cols

--- 2023/13/main.py
+++ 2023/13/main.py
@@ -13,8 +13,25 @@ def main():
             metavar="OUTPUT_FILE", type=argparse.FileType("w"),
             help="path to the output file (write to stdout if omitted)")
     args = parser.parse_args()
+    patterns = []
+    pattern = []
     for line in args.input:
-        print(line_mirrored_splits(line.strip()), file=args.output)
+        if line.strip() == "":
+            patterns.append(pattern)
+            pattern = []
+            continue
+        pattern.append(line.strip())
+    patterns.append(pattern)
+
+    for pattern in patterns:
+        print("rows")
+        for row in pattern:
+            print(line_mirrored_splits(row))
+        print("columns")
+        for idx in range(len(row)):
+            col = list(r[idx] for r in pattern)
+            print(line_mirrored_splits(col))
+    print("", file=args.output)
 
 def line_mirrored_splits(line):
     splits = []
