6f8cdb3162017095f7e98791e989facc5cb844c2
Jacob Kopczynski <ja.kopczynski@gmail.com>
2023-12-17 22:49:59 -0800

    day 18 digging unfilled

--- 2023/18/main.py
+++ 2023/18/main.py
@@ -13,12 +13,36 @@ def main():
             metavar="OUTPUT_FILE", type=argparse.FileType("w"),
             help="path to the output file (write to stdout if omitted)")
     args = parser.parse_args()
-    initial = args.input.readline().strip()
-    dropline = args.input.readline()
-    print(initial, "initial")
+    # total Rs in input ~900, total Ds ~900
+    grid = [['.' for _ in range(12)] for _ in range(12)]
+    # grid = [['.' for _ in range(900)] for _ in range(900)]
+    coords = (0,0)
     for line in args.input:
-        print(line.strip(), file=args.output)
- 
+        direction, distance, color = line.strip().split(" ")
+        distance = int(distance)
+        grid, coords = dig(grid, coords, direction, distance)
+    for row in grid:
+        print(''.join(row), file=args.output)
+    print(sum([len([x for x in row if x == '#']) for row in grid]))
+
+def dig(grid, coords, direction, distance):
+    x, y = coords
+    if direction == 'D':
+        for newy in range(y+1, y+distance+1):
+            grid[newy][x] = "#"
+        return grid, (x, y+distance)
+    elif direction == 'U':
+        for newy in range(y-1, y-distance-1, -1):
+            grid[newy][x] = "#"
+        return grid, (x, y-distance)
+    elif direction == 'R':
+        for newx in range(x+1, x+distance+1):
+            grid[y][newx] = "#"
+        return grid, (x+distance, y)
+    elif direction == 'L':
+        for newx in range(x-1, x-distance-1, -1):
+            grid[y][newx] = "#"
+        return grid, (x-distance, y)
 
 if __name__ == "__main__":
     main()
